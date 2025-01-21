7d304719e824850f404018f50270be419bfef00e
Tejashree1962 <tejugparab555@gmail.com>
2025-01-21 12:47:06 +0530

    Added home result and buy credit pages

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {assets} from '../assets/assets' ;
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
+    </div>
+  )
+}
+
+export default Navbar
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  console.log('Home component rendered');
+
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
