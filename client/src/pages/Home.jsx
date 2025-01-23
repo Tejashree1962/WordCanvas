584985efb48484b9b6aca234d501bd1f170f8b82
Tejashree1962 <tejugparab555@gmail.com>
2025-01-24 00:54:44 +0530

    Header completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Header from '../components/Header';
 
 const Home = () => {
   console.log('Home component rendered');
 
   return (
     <div>
-      
+     <Header/> 
     </div>
   )
 }
