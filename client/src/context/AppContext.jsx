5f770bc78b3047d73be5f1ac3aaaa4d1af791247
Harshita <harshitagosavi2004@gmail.com>
2025-03-10 16:17:40 +0530

    pages done

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -5,11 +5,13 @@ export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
     const [user, setUser] = useState(null); //if true,crdits page shows purchase option
-
+    const [showLogin, setShowLogin] = useState(false);
     // The value to be passed to the context
     const value = {
         user,
         setUser,
+        showLogin,
+        setShowLogin
     };
 
     return (
