367cbd5812a903d2334a9f9f9332d9140242ff93
Shruti More <moreshruti157@gmail.com>
2025-03-11 14:03:08 +0530

    API TESTED

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,12 +6,22 @@ export const AppContext = createContext();
 const AppContextProvider = (props) => {
     const [user, setUser] = useState(null); //if true,crdits page shows purchase option
     const [showLogin, setShowLogin] = useState(false);
+    const [token, setToken ] = useState(localStorage.getItem('token'))
+    const [credit,setCredit ] = useState(false)
+
+    const backendurl = import.meta.env.VITE_BACKEND_URL 
+
     // The value to be passed to the context
     const value = {
         user,
         setUser,
         showLogin,
-        setShowLogin
+        setShowLogin,
+        backendurl,
+        token,
+        setToken,
+        credit,
+        setCredit
     };
 
     return (
